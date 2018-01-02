09991d66461c8a4c7592bf553ccecce6333c2f7d
Mars <talent.seek@msa.hinet.net>
2018-01-02 18:10:47 +0800

    20180102

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -23,27 +23,102 @@
       </div>
     </div>
     <!-- 主題內容 -->
-    <div class="content-wrapper"></div>
+    <div class="content-wrapper">
+      <div class="icon" :style="head_bg">
+      </div>
+      <div class="name">
+        <h3>
+          {{poiInfo.name}}
+        </h3>
+      </div>
+      <div class="collect">
+        <img src="../../../resource/img/star.png" alt="">
+        <span>收藏</span>
+      </div>
+    </div>
     <!-- 公告內容 -->
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <img class="icon" v-if="poiInfo.discounts2" :src="poiInfo.discounts2[0].icon_url" alt="">
+      <span class="text" v-if="poiInfo.discounts2" >{{poiInfo.discounts2[0].info}}</span>
+      <div class="detail" v-if="poiInfo.discounts2" @click="showBulletin">
+        {{poiInfo.discounts2.length}}個活動
+        <span class="icon-keyboard_arrow_right"></span>
+      </div>
+    </div>
     <!-- 背景 -->
     <div class="bg-wrapper" :style="content_bg" >
       <!-- <img :src="poiInfo.head_pic_url" alt=""> -->
     </div>
+
+    <!-- 公告詳情 -->
+     <transition name="detail">
+      <div class="bulletin-detail" v-show="isShow">
+        <div class="detail-wrapper">
+          <div class="main-wrapper" :style="detail_bg">
+            <div class="icon" :style="head_bg"></div>
+            <h3 class="name">{{poiInfo.name}}</h3>
+            <!-- 評價 -->
+
+          <p class="tip">
+            {{poiInfo.min_price_tip}} <i>|</i>
+            {{poiInfo.shipping_fee_tip}} <i>|</i>
+            {{poiInfo.delivery_time_tip}} <i>|</i>
+          </p>
+          <p class="time">
+              配送時間:
+              {{poiInfo.shipping_time}}
+          </p>
+          <div class="discounts" v-if="poiInfo.discounts2">
+              <p>
+                <img :src="poiInfo.discounts2[0].icon_url" alt="">
+
+                <span>{{poiInfo.discounts2[0].info}}</span>
+              </p>
+          </div>
+
+          </div>
+          <div class="close-wrapper">
+            <span class="icon-close" @click="closeBulletin"></span>
+          </div>
+        </div>
+      </div>
+    </transition>
+
   </div>
 </template>
 
 <script>
 export default {
-  props: {    //組件傳值
-    poiInfo:{
+  data() {
+    return {
+      isShow: false //公告詳情預設不顯示
+    };
+  },
+  props: {
+    //組件傳值
+    poiInfo: {
       type: Object,
-      default: {},
+      default: {}
     }
   },
-  computed:{  //計算屬性
-    content_bg(){
+  computed: {
+    //計算屬性
+    content_bg() {
       return `background-image: url(${this.poiInfo.head_pic_url})`;
+    },
+    head_bg() {
+      return `background-image: url(${this.poiInfo.pic_url})`;
+    },
+    detail_bg() {
+      return `background-image: url(${this.poiInfo.poi_back_pic_url})`;
+    }
+  },
+  methods: {
+    showBulletin() {
+      this.isShow = true;
+    },
+    closeBulletin() {
+      this.isShow = false;
     }
   }
 };
@@ -52,8 +127,8 @@ export default {
 
 
 <style>
- /* 導入字體圖標 */
- @import url("../../common/styles/icon.css");
+/* 導入字體圖標 */
+@import url("../../common/styles/icon.css");
 </style>
 
 
